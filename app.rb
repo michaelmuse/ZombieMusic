f1ef25c62de30f33a5828a52c26eb280d6f44dcd
Michael Muse <michaelmuse@gmail.com>
2013-12-16 11:49:36 -0500

    fixing the api key stuff

--- app.rb
+++ app.rb
@@ -11,18 +11,15 @@ MusicBrainz.configure do |c|
   c.contact = "support@mymusicapp.com"
 end
  
- 
-before do
-  lastfm_api_key = ENV['LASTFM_KEY']
-  soundcloud_api_key = ENV['SOUNDCLOUD_KEY']
-end
-
 
 get '/' do
   erb :index
 end
  
 post '/' do
+  lastfm_api_key = ENV['LASTFM_KEY']
+  soundcloud_api_key = ENV['SOUNDCLOUD_KEY'] 
+
   artist = params[:artist]
   @result = MusicBrainz::Artist.find_by_name(artist)
   if @result == nil
@@ -39,7 +36,7 @@ post '/' do
     artist_1_albums_url  = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&mbid=#{@artist_id}&api_key=#{lastfm_api_key}&format=json"
     @artist_1_albums  = HTTParty.get(artist_1_albums_url)
    
-    client = Soundcloud.new(:client_id => 'soundcloud_api_key')
+    client = Soundcloud.new(:client_id => soundcloud_api_key)
    
     clean_artist_name = artist.split(' ').join('').downcase
     # get a tracks oembed data
